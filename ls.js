d2f99f1df28d3a1eab0bce9d130089920edccdc3
gfranca11 <87835304+gfranca11@users.noreply.github.com>
2022-07-01 20:58:34 -0300

    cursos

---
+++ ls.js
@@ -0,0 +1,9 @@
+function clicou(){
+    let menu = document.getElementById("menu-area");
+     
+ if (menu.style.height == '200px' ) {
+     menu.style.height  = '0px';
+ }
+ else{
+     menu.style.height  = '200px';
+ } }
